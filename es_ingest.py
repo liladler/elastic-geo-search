983ea0fa1f71fc3e8df1ffcc7c32672a92740800
Lily Adler <lilyadler@Lilys-MBP.lan>
2024-05-27 19:17:24 +0300

    update

--- es_ingest.py
+++ es_ingest.py
@@ -3,12 +3,13 @@ import os
 import shutil    
 import glob    
 import zipfile    
+from elasticsearch import Elasticsearch, helpers  
 from dotenv import load_dotenv      
-from elasticsearch import Elasticsearch, helpers      
   
 # Specify base directory for the files    
 base_dir = '5749846/'    
-  
+index="tripadvisor_reviews"
+
 # Loop through the base directory and its subdirectories to find and unzip all zip files  
 for dir_name, _, file_list in os.walk(base_dir):    
     for file_name in file_list:    
@@ -31,11 +32,14 @@ for dir_name, _, file_list in os.walk(base_dir):
   
 # Convert all pickle files in new directory to CSV and then remove the pickle files  
 pickle_files = glob.glob(dir_path + '*.pickle')    
-for file in pickle_files:    
-    df = pd.read_pickle(file)    
-    csv_file = file.replace('.pickle', '.csv')    
-    df.to_csv(csv_file, index=False)    
-    os.remove(file)    
+for file in pickle_files:     
+    if os.path.exists(file):   
+        df = pd.read_pickle(file)      
+        csv_file = file.replace('.pickle', '.csv')      
+        df.to_csv(csv_file, index=False)      
+        os.remove(file)  
+    else:  
+        print(f"{file} does not exist. Skipping to next file.")  
   
 # Function to load CSV data into Elasticsearch  
 def csv_to_elasticsearch(es, index_name, dir_path):    
@@ -46,28 +50,32 @@ def csv_to_elasticsearch(es, index_name, dir_path):
         file_path = os.path.join(dir_path, file_name)  # Combine directory path and file name  
         df = pd.read_csv(file_path)  # Load CSV into a DataFrame  
         df.fillna("", inplace=True)  # Replace null values with empty string  
-        
         df_dict = df.to_dict(orient='records')  # Convert DataFrame to dictionary format for Elasticsearch  
   
         # Prepare a list of actions for Elasticsearch bulk API  
         actions = [    
             {    
                 "_index": index_name,  # Specify index name  
-                "_source": row  # Specify document source  
+                 "_source": {**row, "_run_ml_inference": True},  # run through ML inference
+                "pipeline": index_name # Specify pipeline
             }    
             for row in df_dict  # Loop through each row in the DataFrame dictionary  
         ]    
-  
-        helpers.bulk(es, actions)  # Use Elasticsearch bulk API to index documents  
+        try:  
+            helpers.bulk(es, actions)  
+        except helpers.BulkIndexError as e:  
+            for error in e.errors:  
+                print(error)  
   
 # Load environment variables from .env file  
 load_dotenv()    
   
 # Initialize Elasticsearch client with credentials from environment variables  
 es = Elasticsearch(    
-    cloud_id=os.getenv('CLOUD_ID'),    
+    cloud_id=os.getenv('CLOUD_URL'),    
     basic_auth=(os.getenv('ELASTIC_USER'), os.getenv('ELASTIC_PASSWORD'))    
 )     
   
 # Load data from CSV files into Elasticsearch  
-csv_to_elasticsearch(es, "tripadvisor_reviews", dir_path)    
+csv_to_elasticsearch(es, index, dir_path)    
+
